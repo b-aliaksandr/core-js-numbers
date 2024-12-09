d1384e632cba6cd861f5adf55d8db08b2d35f289
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2024-12-09 20:27:15 +0300

    feat: implement roundToPowerOfTen

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -190,8 +190,13 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  if (pow === 0) return num;
+  const secondPartOfNum = num % 10 ** (pow + 1);
+  const halfNumberOfPowerOfTen = 10 ** (pow + 1) / 2;
+  const isLargest = secondPartOfNum >= halfNumberOfPowerOfTen;
+  const firstPartOfNum = Number.parseInt(num / 10 ** pow, 10) * 10 ** pow;
+  return isLargest ? firstPartOfNum + 10 ** pow : firstPartOfNum;
 }
 
 /**
